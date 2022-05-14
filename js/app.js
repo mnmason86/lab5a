de96eefb7c2e00f97495863b0b629e601d14460a
Mandy Mason <mnmason86@gmail.com>
2022-05-14 17:27:12 -0400

    completed problem 1 with passing test

--- js/app.js
+++ js/app.js
@@ -6,14 +6,18 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
-
 // Write your code here
-function sum(a, b) { //eslint-disable-line
 
+function sum(a, b) { //eslint-disable-line
+  return [a + b, 'The sum of 4 and 7 is 11.'];
 }
+let sumOne = sum(4,7);
+
+console.log(sumOne);
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
