b2b738b7c0c47489faeb707ea3f8633ce2259311
Mandy Mason <mnmason86@gmail.com>
2022-05-14 17:50:49 -0400

    completed problem 3 with passing test

--- js/app.js
+++ js/app.js
@@ -31,9 +31,11 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  return [a * b, 'The product of 5 and 9 is 45.'];
 }
 
+let productTwo = multiply(5,9);
+console.log (productTwo);
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
 
@@ -52,11 +54,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  return [a + b + c, a * b * c, '4 and 7 and 5 sum to 16.', 'The product of 4 and 7 and 5 is 140.'];
 }
 
+let sumProduct = sumAndMultiply(4,7,5);
+console.log(sumProduct);
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+ testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
